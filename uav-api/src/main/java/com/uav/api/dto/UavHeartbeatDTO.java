8bd0aefdad9607222a14ffaa31c39c330755132b
chensimi <chensimi2024@163.com>
2026-01-21 23:04:20 +0800

    feat:初次成功调用gateway-iot

--- uav-api/src/main/java/com/uav/api/dto/UavHeartbeatDTO.java
+++ uav-api/src/main/java/com/uav/api/dto/UavHeartbeatDTO.java
@@ -2,8 +2,13 @@ package com.uav.api.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class UavHeartbeatDTO {
+public class UavHeartbeatDTO implements Serializable {
+    // 加上版本号（防止序列化报错）
+    private  static final long serialVersionUID = 1L;
+
     private String deviceId;
     private Long timestamp;
 }
--- uav-service/src/main/java/com/uav/service/UavServiceApplication.java
+++ uav-service/src/main/java/com/uav/service/UavServiceApplication.java
@@ -1,10 +1,16 @@
 package com.uav.service;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        RedisAutoConfiguration.class,
+        RedissonAutoConfiguration.class})
 @EnableDubbo
 public class UavServiceApplication {
     public static void main(String[] args) {
